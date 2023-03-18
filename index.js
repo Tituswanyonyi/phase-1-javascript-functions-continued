64528bd9759954532063dad94427faececaee854
titus <titus.unavailable@student.moringaschool.com>
2023-03-18 10:16:31 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(undertaking="roller-skate") {
+    return `This Saturday, I want to ${undertaking}!`
+  }
+  
+  let mondayWork = function(undertaking="go to the office") {
+    return `This Monday, I will ${undertaking}!`
+  }
+  
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+
